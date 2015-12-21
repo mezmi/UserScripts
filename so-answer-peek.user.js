0d4c96ae81f5a865ab7f1a20e23f1dba2072f6bf
enki <enki-code@users.noreply.github.com>
2015-12-21 15:59:37 -0800

    Update so-answer-peek.user.js

--- so-answer-peek.user.js
+++ so-answer-peek.user.js
@@ -6,17 +6,18 @@
 // @grant        none
 // @run-at document-end
 // ==/UserScript==
-
 /* jshint -W097 */
 'use strict';
 
 $(function () {
-    $("#answers div.answer:first").before("<div id='top-answers-peek' style='width: 400px; margin:0 auto;'><table><tr></tr></table></div>");
+    var count = 0;
+    $("#answers div.answer:first").before("<div id='top-answers-peek' style='margin:0 auto;'><table><tr></tr></table></div>");
     $(".answercell:lt(4)").each(function() {
+        count++;
         var href = $(this).find(".post-menu").find(".short-link").attr("href"),
             score = $(this).prev().find(".vote-count-post").text();
         
         $("#top-answers-peek table tr").append("<td style='width: 100px; text-align: center;'><a href=" + href + "><i class='vote-up-on'></i> Score: " + score + "</a></td>");
-        
     });
+    $("#top-answers-peek").css("width", count * 100 + "px");
 });

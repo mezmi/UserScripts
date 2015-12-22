2b5d2a71a4bb031796683d87c179598377a50a80
enki <enki-code@users.noreply.github.com>
2015-12-21 16:06:36 -0800

    Update so-answer-peek.user.js

--- so-answer-peek.user.js
+++ so-answer-peek.user.js
@@ -8,16 +8,19 @@
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
-
 $(function () {
     var count = 0;
     $("#answers div.answer:first").before("<div id='top-answers-peek' style='margin:0 auto;'><table><tr></tr></table></div>");
     $(".answercell:lt(4)").each(function() {
         count++;
         var href = $(this).find(".post-menu").find(".short-link").attr("href"),
-            score = $(this).prev().find(".vote-count-post").text();
+            score = $(this).prev().find(".vote-count-post").text(),
+            icon = "vote-up-off";
         
-        $("#top-answers-peek table tr").append("<td style='width: 100px; text-align: center;'><a href=" + href + "><i class='vote-up-on'></i> Score: " + score + "</a></td>");
+        if (score > 0) {icon = "vote-up-on";}
+        if (score < 0) {icon = "vote-down-on";}
+        
+        $("#top-answers-peek table tr").append("<td style='width: 100px; text-align: center;'><a href=" + href + "><i class='" + icon + "' style='margin-bottom: 0;'></i> Score: " + score + "</a></td>");
     });
     $("#top-answers-peek").css("width", count * 100 + "px");
 });

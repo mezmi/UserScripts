633f02a48ca86413f4dab1d5df6d65ee0df24400
enki <enki-code@users.noreply.github.com>
2016-01-28 13:51:44 -0800

    Update answer-peek.user.js

--- answer-peek.user.js
+++ answer-peek.user.js
@@ -14,19 +14,27 @@
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
+
 $(function () {
     var count = 0;
     $("#answers div.answer:first").before("<div id='top-answers-peek' style='padding-bottom: 10px; border-bottom: 1px solid #eaebec; display: none;'><table style='margin:0 auto; '><tr></tr></table></div>");
-    $(".answercell").slice( 1, 5 ).each(function() {
+
+    function score(e) {
+      return new Number($(e).parent().find('.vote-count-post').text());
+    }
+    function compareByScore(a, b) {
+      return score(b) - score(a);
+    }
+    $(":not(.deleted-answer) .answercell").slice(1).sort(compareByScore).slice(0, 5).each(function() {
         count++;
-        var href = $(this).find(".short-link").attr("href"),
+        var id = $(this).find(".short-link").attr("id").replace('link-post-', ''),
             score = $(this).prev().find(".vote-count-post").text(),
             icon = "vote-up-off";
-        
+
         if (score > 0) {icon = "vote-up-on";}
         if (score < 0) {icon = "vote-down-on";}
-        
-        $("#top-answers-peek table tr").append("<td style='width: 100px; text-align: center;'><a href=" + href + "><i class='" + icon + "' style='margin-bottom: 0;'></i> Score: " + score + "</a></td>");
+
+        $("#top-answers-peek table tr").append("<td style='width: 100px; text-align: center;'><a href='#" + id + "'><i class='" + icon + "' style='margin-bottom: 0;'></i> Score: " + score + "</a></td>");
     });
     if (count > 0){
         $("#top-answers-peek").show();
